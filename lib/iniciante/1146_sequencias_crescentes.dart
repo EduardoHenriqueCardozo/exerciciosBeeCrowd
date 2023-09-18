50b1ca101ca79f3be1837a582de117d2d039cf55
EduardoHenriqueCardozo <eduardo-henrique-2008@hotmail.com>
2023-09-18 10:06:25 -0300

    Exercicio 1146 modificado

--- lib/iniciante/1146_sequencias_crescentes.dart
+++ lib/iniciante/1146_sequencias_crescentes.dart
@@ -1,14 +1,13 @@
 import 'dart:io';
 
 void main() {
-  bool continuar = true;
+  bool continuar = false;
   do {
     int x = int.parse(stdin.readLineSync()!);
 
     if (x == 0) {
       continuar = false;
-      return;
-    } else {
+    } else if(x > 0) {
       for (var i = 1; i <= x; i++) {
         if (i == x) {
           stdout.write('$i');
@@ -16,6 +15,17 @@ void main() {
           stdout.write('$i ');
         }
       }
+      continuar = true;
+      print('');
+    } else if(x < 0){
+      for (var i = -1; i >= x; i--) {
+        if (i == x) {
+          stdout.write('$i');
+        } else {
+          stdout.write('$i ');
+        }
+      }
+      continuar = true;
       print('');
     }
   } while (continuar == true);
